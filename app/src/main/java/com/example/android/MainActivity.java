2f853942d1a81974a7a5cca3173dfd81ed24af6b
李念寰 <neil25802@gmail.com>
2022-10-03 20:21:21 +0800

    Three types

---
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private View view;
+
+    TextView tx;
+    Button bt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tx = findViewById(R.id.textView);
+        bt = findViewById(R.id.button);
+        bt.setOnClickListener(this);
+        tx.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button :
+                tx.setTextColor(Color.YELLOW) ;
+                break;
+            case R.id.textView :
+                tx.setTextColor(Color.BLUE) ;
+                break;
+        }
+    }
+
+
+
+
+
+
+
+
+
+//    private View.OnClickListener myClickListener = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//            switch (view.getId()) {
+//                case R.id.button :
+//                    tx.setTextColor(Color.GREEN) ;
+//                    break;
+//                case R.id.textView :
+//                    tx.setTextColor(Color.BLACK) ;
+//                    break;
+//            }
+//        }
+//    };
+
+//    public void onButtonClick( View view ) {
+//        tx.setTextColor(Color.RED);
+//    }
+
+}
