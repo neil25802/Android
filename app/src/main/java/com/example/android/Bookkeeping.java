91f285a5ce185391bfdd4259d8b5c33661b9d430
李念寰 <neil25802@gmail.com>
2022-11-28 11:20:33 +0800

    切換App

--- app/src/main/java/com/example/android/Bookkeeping.java
+++ app/src/main/java/com/example/android/Bookkeeping.java
@@ -2,7 +2,6 @@ package com.example.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,34 +9,53 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class Bookkeeping extends AppCompatActivity implements View.OnClickListener {
 
     final String TAG = this.getClass().getSimpleName();
     private View view;
 
+    TextView theDate, theTime;
 //    Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.bookkeeping_constraintlayout);
 
         Log.d(TAG, "enter onCreate()");
+        uiInit();
+    }
+    private void uiInit () {
+        theDate = findViewById(R.id.textView3);
+        theTime = findViewById(R.id.textView7);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        // android.R.id.content : View Root
-        // 設定click監聽
-        findViewById(android.R.id.content).setOnClickListener(this);
+        varInit();
+        setUiListener();
         Log.d(TAG, "enter onStart()");
+    }
+
+    private void setUiListener() {
+        theDate.setOnClickListener(this);
+        theTime.setOnClickListener(this);
+    }
+
+    private void varInit() {
     }
 
     @Override
     protected void onStop() {
         Log.d(TAG, "enter onStop()");
+        releaseUiListener();
         super.onStop();
+    }
+
+    private void releaseUiListener() {
+        theDate.setOnClickListener(null);
+        theTime.setOnClickListener(null);
     }
 
     @Override
@@ -66,16 +84,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()) {
-            case android.R.id.content :
-                startActivity(new Intent(this, Bookkeeping.class));
-
-                //增加過場動畫
-                overridePendingTransition(android.R.anim.slide_in_left,
-                        android.R.anim.slide_out_right);
-
-                //強制activity終止
-                MainActivity.this.finish();
-        }
     }
 }
